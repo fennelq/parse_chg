609f7716b09c2ab3f87ab22a1a82486d154b70c0
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-02 20:00:40 +0700

    Replace etnames.et read macros with function

--- src/sig/etnames_et.rs
+++ src/sig/etnames_et.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct EtnamesEt {
     flag_line: [u8; 2],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for EtnamesEt {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for EtnamesEt {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for EtnamesEt {
     }
 }
 
-named!(pub read_etnames_et<&[u8], EtnamesEt>,
+/*named!(pub read_etnames_et<&[u8], EtnamesEt>,
     complete!(do_parse!(
         tag!("etnames.et")                  >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_etnames_et<&[u8], EtnamesEt>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_etnames_et(i: &[u8]) -> IResult<&[u8], EtnamesEt> {
+    let (i, _) = tag("etnames.et")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(2u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        EtnamesEt {
+            flag_line: *array_ref!(flag_line, 0, 2),
+            source: source.to_vec(),
+        },
+    ))
+}
