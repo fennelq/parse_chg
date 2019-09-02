cf4b1111017147ed25a66a6c2c51fa74d29681ef
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-02 19:58:22 +0700

    Replace elsss.fe read macros with function

--- src/sig/elsss_fe.rs
+++ src/sig/elsss_fe.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ElsssFe {
     flag_line: [u8; 4],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for ElsssFe {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for ElsssFe {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for ElsssFe {
     }
 }
 
-named!(pub read_elsss_fe<&[u8], ElsssFe>,
+/*named!(pub read_elsss_fe<&[u8], ElsssFe>,
     complete!(do_parse!(
         tag!("elsss.fe")                    >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_elsss_fe<&[u8], ElsssFe>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_elsss_fe(i: &[u8]) -> IResult<&[u8], ElsssFe> {
+    let (i, _) = tag("elsss.fe")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(4u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        ElsssFe {
+            flag_line: *array_ref!(flag_line, 0, 4),
+            source: source.to_vec(),
+        },
+    ))
+}
