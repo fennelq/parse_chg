29044a233eac798687f17261d90da3d085bcf964
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 00:53:47 +0700

    Replace File type read macros with function

--- src/sig/file_type.rs
+++ src/sig/file_type.rs
@@ -1,10 +1,12 @@
+use nom::{branch::alt, bytes::complete::tag, IResult};
 use std::fmt;
+use std::fs::File;
 
 #[derive(Debug)]
 pub enum FileType {
     BUILDER011, //monomakh 4.5
     CHARGE37,   //monomakh-SAPR 2013
-    ERROR       //another title
+    ERROR,      //another title
 }
 impl fmt::Display for FileType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -16,7 +18,7 @@ impl fmt::Display for FileType {
     }
 }
 
-named!(pub read_file_type<&[u8], FileType>,
+/*named!(pub read_file_type<&[u8], FileType>,
     alt_complete!(
         map!(tag!("BUILDER011"),
                  |_| FileType::BUILDER011)  |
@@ -24,4 +26,20 @@ named!(pub read_file_type<&[u8], FileType>,
                  |_| FileType::CHARGE37)    |
         map!(tag!(""), |_| FileType::ERROR)
     )
-);
+);*/
+
+pub fn read_file_type(i: &[u8]) -> IResult<&[u8], FileType> {
+    let mut part = [0; 10];
+    let mut f: FileType = FileType::ERROR;
+    part.clone_from_slice(&i[0..9]);
+    if &part[0..9] == b"BUILDER011" {
+        f = FileType::BUILDER011;
+        let (i, _) = tag("BUILDER011")(i)?;
+    } else if &part[0..7] == b"CHARGE37" {
+        f = FileType::CHARGE37;
+        let (i, _) = tag("CHARGE37")(i)?;
+    } else {
+        f = FileType::ERROR;
+    }
+    Ok((i, f))
+}
