22f7218ace46ee410f44fe538705b3788294a7e3
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:03:00 +0700

    Replace rab.sdr read macros with function

--- src/sig/rab_sdr.rs
+++ src/sig/rab_sdr.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct RabSdr {
     flag_line: [u8; 5],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for RabSdr {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for RabSdr {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,6 +39,7 @@ impl fmt::Display for RabSdr {
     }
 }
 
+/*
 named!(pub read_rab_sdr<&[u8], RabSdr>,
     complete!(do_parse!(
         tag!("rab.sdr")                     >>
@@ -46,3 +53,19 @@ named!(pub read_rab_sdr<&[u8], RabSdr>,
         })
     ))
 );
+*/
+
+pub fn read_rab_sdr(i: &[u8]) -> IResult<&[u8], RabSdr> {
+    let (i, _) = tag("rab.sdr")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(5u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        RabSdr {
+            flag_line: *array_ref!(flag_line, 0, 5),
+            source: source.to_vec(),
+        },
+    ))
+}
