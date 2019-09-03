d11bb6c1142dc082c0da4eb8c4b4863b033e7267
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 09:41:33 +0700

    Replace loadcomb.cds read macros with function

--- src/sig/loadcomb_cds.rs
+++ src/sig/loadcomb_cds.rs
@@ -1,10 +1,14 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct LoadcombCds {
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for LoadcombCds {
     fn write(&self) -> Vec<u8> {
@@ -24,7 +28,7 @@ impl fmt::Display for LoadcombCds {
     }
 }
 
-named!(pub read_loadcomb_cds<&[u8], LoadcombCds>,
+/*named!(pub read_loadcomb_cds<&[u8], LoadcombCds>,
     complete!(do_parse!(
         tag!("loadcomb.cds")                >>
         take!(1)                            >>
@@ -34,4 +38,17 @@ named!(pub read_loadcomb_cds<&[u8], LoadcombCds>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_loadcomb_cds(i: &[u8]) -> IResult<&[u8], LoadcombCds> {
+    let (i, _) = tag("loadcomb.cds")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        LoadcombCds {
+            source: source.to_vec(),
+        },
+    ))
+}
