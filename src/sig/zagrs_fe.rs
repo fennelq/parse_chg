9ddcf030b7d9cb6af9fad2db771b73348f222356
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:26:41 +0700

    Replace zagrs.fe read macros with function

--- src/sig/zagrs_fe.rs
+++ src/sig/zagrs_fe.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ZagrsFe {
     flag_line: [u8; 4],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for ZagrsFe {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for ZagrsFe {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for ZagrsFe {
     }
 }
 
-named!(pub read_zagrs_fe<&[u8], ZagrsFe>,
+/*named!(pub read_zagrs_fe<&[u8], ZagrsFe>,
     complete!(do_parse!(
         tag!("zagrs.fe")                    >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_zagrs_fe<&[u8], ZagrsFe>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_zagrs_fe(i: &[u8]) -> IResult<&[u8], ZagrsFe> {
+    let (i, _) = tag("zagrs.fe")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(4u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        ZagrsFe {
+            flag_line: *array_ref!(flag_line, 0, 4),
+            source: source.to_vec(),
+        },
+    ))
+}
