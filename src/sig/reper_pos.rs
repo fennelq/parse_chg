99c68dec87a08361959262dc92aaeacbfa25688c
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:08:16 +0700

    Replace reper.pos read macros with function

--- src/sig/reper_pos.rs
+++ src/sig/reper_pos.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ReperPos {
     flag_line: [u8; 3],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for ReperPos {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for ReperPos {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for ReperPos {
     }
 }
 
-named!(pub read_reper_pos<&[u8], ReperPos>,
+/*named!(pub read_reper_pos<&[u8], ReperPos>,
     complete!(do_parse!(
         tag!("reper.pos")                   >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_reper_pos<&[u8], ReperPos>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_reper_pos(i: &[u8]) -> IResult<&[u8], ReperPos> {
+    let (i, _) = tag("reper.pos")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(3u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        ReperPos {
+            flag_line: *array_ref!(flag_line, 0, 3),
+            source: source.to_vec(),
+        },
+    ))
+}
