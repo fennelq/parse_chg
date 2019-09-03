37497f2d322d0fce53fe54b32ccec35aad03fdca
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 09:47:57 +0700

    Replace nodesres.fe read macros with function

--- src/sig/nodesres_fe.rs
+++ src/sig/nodesres_fe.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct NodesresFe {
     flag_line: [u8; 1],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for NodesresFe {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for NodesresFe {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for NodesresFe {
     }
 }
 
-named!(pub read_nodesres_fe<&[u8], NodesresFe>,
+/*named!(pub read_nodesres_fe<&[u8], NodesresFe>,
     complete!(do_parse!(
         tag!("nodesres.fe")                 >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_nodesres_fe<&[u8], NodesresFe>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_nodesres_fe(i: &[u8]) -> IResult<&[u8], NodesresFe> {
+    let (i, _) = tag("nodesres.fe")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(1u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        NodesresFe {
+            flag_line: *array_ref!(flag_line, 0, 1),
+            source: source.to_vec(),
+        },
+    ))
+}
