3fc19bc0311a0a17aa310e3f95b85151dbe3fa57
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 09:51:46 +0700

    Replace object.nam read macros with function

--- src/sig/object_nam.rs
+++ src/sig/object_nam.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ObjectNam {
     flag_line: [u8; 2],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for ObjectNam {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for ObjectNam {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for ObjectNam {
     }
 }
 
-named!(pub read_object_nam<&[u8], ObjectNam>,
+/*named!(pub read_object_nam<&[u8], ObjectNam>,
     complete!(do_parse!(
         tag!("object.nam")                  >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_object_nam<&[u8], ObjectNam>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_object_nam(i: &[u8]) -> IResult<&[u8], ObjectNam> {
+    let (i, _) = tag("object.nam")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(2u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        ObjectNam {
+            flag_line: *array_ref!(flag_line, 0, 2),
+            source: source.to_vec(),
+        },
+    ))
+}
