b6127d4e54b20f85827d233141f72ad6d80cdd10
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 18:16:32 +0700

    Replace rab.e\beam read macros with function

--- src/sig/rab_e/beam.rs
+++ src/sig/rab_e/beam.rs
@@ -1,8 +1,8 @@
 //! Балки
-use nom::{le_u8};
-use std::fmt;
-use crate::sig::rab_e::*;
 use crate::sig::rab_e::sec::*;
+use crate::sig::rab_e::*;
+use nom::{bytes::complete::take, number::complete::le_u8, IResult};
+use std::fmt;
 
 #[derive(Debug)]
 /// Балка
@@ -16,15 +16,19 @@ pub struct Beam {
     type_sec: u8,
     ws2: Vec<u8>, //41b
     /// Тип сечения балки
-    sec: Sec
+    sec: Sec,
 }
 impl fmt::Display for Beam {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "p1 |{}|, p2 |{}|, Sec №{}, {}",
-               &self.p1, &self.p2, &self.type_sec, &self.sec)
+        write!(
+            f,
+            "p1 |{}|, p2 |{}|, Sec №{}, {}",
+            &self.p1, &self.p2, &self.type_sec, &self.sec
+        )
     }
 }
 
+/*
 named!(pub read_beam<&[u8], Beam>,
     do_parse!(
         p1: read_point                      >>
@@ -42,4 +46,24 @@ named!(pub read_beam<&[u8], Beam>,
             sec
         })
     )
-);
+);*/
+
+pub fn read_beam(i: &[u8]) -> IResult<&[u8], Beam> {
+    let (i, p1) = read_point(i)?;
+    let (i, p2) = read_point(i)?;
+    let (i, ws1) = take(36u8)(i)?;
+    let (i, type_sec) = le_u8(i)?;
+    let (i, ws2) = take(41u8)(i)?;
+    let (i, sec) = read_sec(i, type_sec)?;
+    Ok((
+        i,
+        Beam {
+            p1,
+            p2,
+            ws1: ws1.to_vec(),
+            type_sec,
+            ws2: ws2.to_vec(),
+            sec,
+        },
+    ))
+}
