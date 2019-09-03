a8554e7d7f253b534245dde2ee65e66391494f8c
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 18:30:45 +0700

    Replace rab.e\fbeam read macros with function

--- src/sig/rab_e/f_beam.rs
+++ src/sig/rab_e/f_beam.rs
@@ -1,8 +1,12 @@
 //! Фундаментные балки
-use nom::{le_u8, le_u16};
-use std::fmt;
-use crate::sig::rab_e::*;
 use crate::sig::rab_e::sec::*;
+use crate::sig::rab_e::*;
+use nom::{
+    bytes::complete::take,
+    number::complete::{le_u16, le_u8},
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct FBeam {
@@ -12,16 +16,18 @@ pub struct FBeam {
     xz1: u16,
     type_sec: u8,
     ws2: Vec<u8>, //40b
-    sec: Sec
+    sec: Sec,
 }
 impl fmt::Display for FBeam {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "p1 |{}|, p2 |{}|, zx1: {}, sec №{}, {}",
-               &self.p1, &self.p2, &self.xz1,
-               &self.type_sec, &self.sec)
+        write!(
+            f,
+            "p1 |{}|, p2 |{}|, zx1: {}, sec №{}, {}",
+            &self.p1, &self.p2, &self.xz1, &self.type_sec, &self.sec
+        )
     }
 }
-
+/*
 named!(pub read_fbeam<&[u8], FBeam>,
     do_parse!(
         p1: read_point                      >>
@@ -41,4 +47,26 @@ named!(pub read_fbeam<&[u8], FBeam>,
             sec
         })
     )
-);
+);*/
+
+pub fn read_fbeam(i: &[u8]) -> IResult<&[u8], FBeam> {
+    let (i, p1) = read_point(i)?;
+    let (i, p2) = read_point(i)?;
+    let (i, ws1) = take(2u8)(i)?;
+    let (i, xz1) = le_u16(i)?;
+    let (i, type_sec) = le_u8(i)?;
+    let (i, ws2) = take(40u8)(i)?;
+    let (i, sec) = read_sec(i, type_sec)?;
+    Ok((
+        i,
+        FBeam {
+            p1,
+            p2,
+            ws1: *array_ref!(ws1, 0, 2),
+            xz1,
+            type_sec,
+            ws2: ws2.to_vec(),
+            sec,
+        },
+    ))
+}
