efb22369a72e228420f5a96d0aafa9016a2ce5df
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:23:55 +0700

    Replace wind.rsp read macros with function

--- src/sig/wind_rsp.rs
+++ src/sig/wind_rsp.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct WindRsp {
     flag_line: [u8; 4],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for WindRsp {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for WindRsp {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for WindRsp {
     }
 }
 
-named!(pub read_wind_rsp<&[u8], WindRsp>,
+/*named!(pub read_wind_rsp<&[u8], WindRsp>,
     complete!(do_parse!(
         tag!("wind.rsp")                    >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_wind_rsp<&[u8], WindRsp>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_wind_rsp(i: &[u8]) -> IResult<&[u8], WindRsp> {
+    let (i, _) = tag("wind.rsp")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(4u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        WindRsp {
+            flag_line: *array_ref!(flag_line, 0, 4),
+            source: source.to_vec(),
+        },
+    ))
+}
