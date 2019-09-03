fb387aa957026a6c115613db104f38b45656e3e1
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:20:53 +0700

    Replace vnum.fe read macros with function

--- src/sig/vnum_fe.rs
+++ src/sig/vnum_fe.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct VnumFe {
     flag_line: [u8; 5],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for VnumFe {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for VnumFe {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for VnumFe {
     }
 }
 
-named!(pub read_vnum_fe<&[u8], VnumFe>,
+/*named!(pub read_vnum_fe<&[u8], VnumFe>,
     complete!(do_parse!(
         tag!("vnum.fe")                     >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_vnum_fe<&[u8], VnumFe>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_vnum_fe(i: &[u8]) -> IResult<&[u8], VnumFe> {
+    let (i, _) = tag("vnum.fe")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(5u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        VnumFe {
+            flag_line: *array_ref!(flag_line, 0, 5),
+            source: source.to_vec(),
+        },
+    ))
+}
