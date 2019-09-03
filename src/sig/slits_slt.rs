309177fa66e862ae81b0358a53052a697ff0ae09
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:17:58 +0700

    Replace slits.slt read macros with function

--- src/sig/slits_slt.rs
+++ src/sig/slits_slt.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct SlitsSlt {
     flag_line: [u8; 3],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for SlitsSlt {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for SlitsSlt {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for SlitsSlt {
     }
 }
 
-named!(pub read_slits_slt<&[u8], SlitsSlt>,
+/*named!(pub read_slits_slt<&[u8], SlitsSlt>,
     complete!(do_parse!(
         tag!("slits.slt")                   >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_slits_slt<&[u8], SlitsSlt>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_slits_slt(i: &[u8]) -> IResult<&[u8], SlitsSlt> {
+    let (i, _) = tag("slits.slt")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(3u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        SlitsSlt {
+            flag_line: *array_ref!(flag_line, 0, 3),
+            source: source.to_vec(),
+        },
+    ))
+}
