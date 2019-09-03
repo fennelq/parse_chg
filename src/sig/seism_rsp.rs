b2531118be8095b581a2fab51a33c87cb366afd1
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 10:16:36 +0700

    Replace seism.rsp read macros with function

--- src/sig/seism_rsp.rs
+++ src/sig/seism_rsp.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct SeismRsp {
     flag_line: [u8; 3],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for SeismRsp {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for SeismRsp {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,6 +39,7 @@ impl fmt::Display for SeismRsp {
     }
 }
 
+/*
 named!(pub read_seism_rsp<&[u8], SeismRsp>,
     complete!(do_parse!(
         tag!("seism.rsp")                   >>
@@ -46,3 +53,19 @@ named!(pub read_seism_rsp<&[u8], SeismRsp>,
         })
     ))
 );
+*/
+
+pub fn read_seism_rsp(i: &[u8]) -> IResult<&[u8], SeismRsp> {
+    let (i, _) = tag("seism.rsp")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(3u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        SeismRsp {
+            flag_line: *array_ref!(flag_line, 0, 3),
+            source: source.to_vec(),
+        },
+    ))
+}
