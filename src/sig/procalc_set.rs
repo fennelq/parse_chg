e428be647ec589fb8b7fc80d492feab7b5cbf757
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 09:55:59 +0700

    Replace procalc.set read macros with function

--- src/sig/procalc_set.rs
+++ src/sig/procalc_set.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct ProcalcSet {
     flag_line: [u8; 1],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for ProcalcSet {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for ProcalcSet {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for ProcalcSet {
     }
 }
 
-named!(pub read_procalc_set<&[u8], ProcalcSet>,
+/*named!(pub read_procalc_set<&[u8], ProcalcSet>,
     complete!(do_parse!(
         tag!("procalc.set")                 >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_procalc_set<&[u8], ProcalcSet>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_procalc_set(i: &[u8]) -> IResult<&[u8], ProcalcSet> {
+    let (i, _) = tag("procalc.set")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(1u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        ProcalcSet {
+            flag_line: *array_ref!(flag_line, 0, 1),
+            source: source.to_vec(),
+        },
+    ))
+}
