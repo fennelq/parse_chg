936c31f701d489c54066a341e710318f1ad8b485
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-03 09:53:06 +0700

    Replace pop.cut read macros with function

--- src/sig/pop_cut.rs
+++ src/sig/pop_cut.rs
@@ -1,11 +1,15 @@
-use nom::le_u64;
-use std::fmt;
 use crate::sig::*;
+use nom::{
+    bytes::complete::{tag, take},
+    number::complete::le_u64,
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 pub struct PopCut {
     flag_line: [u8; 5],
-    source: Vec<u8>
+    source: Vec<u8>,
 }
 impl HasWrite for PopCut {
     fn write(&self) -> Vec<u8> {
@@ -25,7 +29,9 @@ impl fmt::Display for PopCut {
         let vec = &self.flag_line;
         write!(f, "{} flag_line: [", &self.name())?;
         for (count, v) in vec.iter().enumerate() {
-            if count != 0 { write!(f, ", ")?; }
+            if count != 0 {
+                write!(f, ", ")?;
+            }
             write!(f, "{}", v)?;
         }
         write!(f, "]; ")?;
@@ -33,7 +39,7 @@ impl fmt::Display for PopCut {
     }
 }
 
-named!(pub read_pop_cut<&[u8], PopCut>,
+/*named!(pub read_pop_cut<&[u8], PopCut>,
     complete!(do_parse!(
         tag!("pop.cut")                     >>
         take!(1)                            >>
@@ -45,4 +51,19 @@ named!(pub read_pop_cut<&[u8], PopCut>,
             source: source.to_vec()
         })
     ))
-);
+);*/
+
+pub fn read_pop_cut(i: &[u8]) -> IResult<&[u8], PopCut> {
+    let (i, _) = tag("pop.cut")(i)?;
+    let (i, _) = take(1u8)(i)?;
+    let (i, flag_line) = take(5u8)(i)?;
+    let (i, offset) = le_u64(i)?;
+    let (i, source) = take(offset)(i)?;
+    Ok((
+        i,
+        PopCut {
+            flag_line: *array_ref!(flag_line, 0, 5),
+            source: source.to_vec(),
+        },
+    ))
+}
