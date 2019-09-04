daffbe38ccad218271c3129fcf54f8830102f30a
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-04 17:32:43 +0700

    Replace rab.e\poly read macros with function

--- src/sig/rab_e/poly.rs
+++ src/sig/rab_e/poly.rs
@@ -1,5 +1,9 @@
 //! Полилинии
-use nom::{le_u8, le_u16};
+use nom::{
+    bytes::complete::take,
+    number::complete::{le_u16, le_u8},
+    IResult,
+};
 use std::fmt;
 
 #[derive(Debug)]
@@ -11,17 +15,19 @@ pub struct Poly {
     ws1: [u8; 4],
     typ: u8,
     number: u16,
-    ws2: [u8; 8]
+    ws2: [u8; 8],
 }
 impl fmt::Display for Poly {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "name: {}, |{} -> {}| ({}), typ: {}, №{}",
-               &self.name, &self.from, &self.to,
-               &self.amount, &self.typ, &self.number)
+        write!(
+            f,
+            "name: {}, |{} -> {}| ({}), typ: {}, №{}",
+            &self.name, &self.from, &self.to, &self.amount, &self.typ, &self.number
+        )
     }
 }
 
-named!(pub read_poly<&[u8], Poly>,
+/*named!(pub read_poly<&[u8], Poly>,
     do_parse!(
         name: le_u16                        >>
         from: le_u16                        >>
@@ -42,4 +48,28 @@ named!(pub read_poly<&[u8], Poly>,
             ws2: *array_ref!(ws2, 0 ,8)
         })
     )
-);
+);*/
+
+pub fn read_poly(i: &[u8]) -> IResult<&[u8], Poly> {
+    let (i, name) = le_u16(i)?;
+    let (i, from) = le_u16(i)?;
+    let (i, to) = le_u16(i)?;
+    let (i, amount) = le_u16(i)?;
+    let (i, ws1) = take(4u8)(i)?;
+    let (i, typ) = le_u8(i)?;
+    let (i, number) = le_u16(i)?;
+    let (i, ws2) = take(8u8)(i)?;
+    Ok((
+        i,
+        Poly {
+            name,
+            from,
+            to,
+            amount,
+            ws1: *array_ref!(ws1, 0, 4),
+            typ,
+            number,
+            ws2: *array_ref!(ws2, 0, 8),
+        },
+    ))
+}
