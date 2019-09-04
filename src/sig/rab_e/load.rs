04ccd6c6c2eecc19f13117fe41f86f84f3d91a37
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-04 16:51:24 +0700

    Replace rab.e\load read macros with function

--- src/sig/rab_e/load.rs
+++ src/sig/rab_e/load.rs
@@ -1,9 +1,10 @@
 //! Загружения
+use nom::{bytes::complete::take, IResult};
 use std::fmt;
 
 #[derive(Debug)]
 pub struct Load {
-    source: Vec<u8> //31b
+    source: Vec<u8>, //31b
 }
 impl fmt::Display for Load {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -11,6 +12,7 @@ impl fmt::Display for Load {
     }
 }
 
+/*
 named!(pub read_load<&[u8], Load>,
     do_parse!(
         source: take!(31)                   >>
@@ -18,4 +20,14 @@ named!(pub read_load<&[u8], Load>,
             source: source.to_vec() //31b
         })
     )
-);
+);*/
+
+pub fn read_load(i: &[u8]) -> IResult<&[u8], Load> {
+    let (i, source) = take(31u8)(i)?;
+    Ok((
+        i,
+        Load {
+            source: source.to_vec(), //31b
+        },
+    ))
+}
