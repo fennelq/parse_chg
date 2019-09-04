e633f549db45715ba6a9e98254f7467b699ac568
Mikhail Boldyshev <fennelq@gmail.com>
2019-09-04 17:29:34 +0700

    Replace rab.e\pile read macros with function

--- src/sig/rab_e/pile.rs
+++ src/sig/rab_e/pile.rs
@@ -1,27 +1,31 @@
 //! Сваи
-use nom::{le_u8, le_f32};
-use std::fmt;
 use crate::sig::rab_e::*;
+use nom::{
+    bytes::complete::take,
+    number::complete::{le_f32, le_u8},
+    IResult,
+};
+use std::fmt;
 
 #[derive(Debug)]
 enum PileType {
-    PileEF(PileEF),
-    PileFL(PileFL),
-    PileSize(PileSize),
+    EF(PileEF),
+    FL(PileFL),
+    Size(PileSize),
 }
 impl fmt::Display for PileType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self {
-            PileType::PileEF(r)   => write!(f, "Type: EF |{}|", r),
-            PileType::PileFL(r)   => write!(f, "Type: F-L |{}|", r),
-            PileType::PileSize(r) => write!(f, "Type: size |{}|", r),
+            PileType::EF(r) => write!(f, "Type: EF |{}|", r),
+            PileType::FL(r) => write!(f, "Type: F-L |{}|", r),
+            PileType::Size(r) => write!(f, "Type: size |{}|", r),
         }
     }
 }
 #[derive(Debug)]
 pub struct PileEF {
     ef: f32,
-    ws1: [u8; 2]
+    ws1: [u8; 2],
 }
 impl fmt::Display for PileEF {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -32,7 +36,7 @@ impl fmt::Display for PileEF {
 pub struct PileFL {
     f: f32,
     delta_l: f32,
-    ws1: [u8; 2]
+    ws1: [u8; 2],
 }
 impl fmt::Display for PileFL {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -49,13 +53,15 @@ pub struct PileSize {
     ws1: [u8; 9],
     b: f32,
     h: f32,
-    ws2: [u8; 2]
+    ws2: [u8; 2],
 }
 impl fmt::Display for PileSize {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "xz1: {}, l: {}, xz2: {}, broaden: {}, k: {}, b: {}, h: {}",
-               &self.xz1, &self.l, &self.xz2, &self.broaden,
-               &self.k, &self.b, &self.h)
+        write!(
+            f,
+            "xz1: {}, l: {}, xz2: {}, broaden: {}, k: {}, b: {}, h: {}",
+            &self.xz1, &self.l, &self.xz2, &self.broaden, &self.k, &self.b, &self.h
+        )
     }
 }
 #[derive(Debug)]
@@ -64,16 +70,19 @@ pub struct Pile {
     p: Point,
     type_pile: u8,
     ws2: [u8; 15],
-    base: PileType
+    base: PileType,
 }
 impl fmt::Display for Pile {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "p |{}|, type №{}, {}",
-               &self.p, &self.type_pile, &self.base)
+        write!(
+            f,
+            "p |{}|, type №{}, {}",
+            &self.p, &self.type_pile, &self.base
+        )
     }
 }
 
-named!(read_pile_ef<&[u8], PileEF>,
+/*named!(read_pile_ef<&[u8], PileEF>,
     do_parse!(
         ef: le_f32                          >>
         ws1: take!(2)                       >>
@@ -82,8 +91,20 @@ named!(read_pile_ef<&[u8], PileEF>,
             ws1: *array_ref!(ws1, 0 ,2)
         })
     )
-);
-named!(read_pile_f_l<&[u8], PileFL>,
+);*/
+pub fn read_pile_ef(i: &[u8]) -> IResult<&[u8], PileEF> {
+    let (i, ef) = le_f32(i)?;
+    let (i, ws1) = take(2u8)(i)?;
+    Ok((
+        i,
+        PileEF {
+            ef,
+            ws1: *array_ref!(ws1, 0, 2),
+        },
+    ))
+}
+
+/*named!(read_pile_f_l<&[u8], PileFL>,
     do_parse!(
         f: le_f32                           >>
         delta_l: le_f32                     >>
@@ -94,8 +115,22 @@ named!(read_pile_f_l<&[u8], PileFL>,
             ws1: *array_ref!(ws1, 0 ,2)
         })
     )
-);
-named!(read_pile_size<&[u8], PileSize>,
+);*/
+pub fn read_pile_f_l(i: &[u8]) -> IResult<&[u8], PileFL> {
+    let (i, f) = le_f32(i)?;
+    let (i, delta_l) = le_f32(i)?;
+    let (i, ws1) = take(2u8)(i)?;
+    Ok((
+        i,
+        PileFL {
+            f,
+            delta_l,
+            ws1: *array_ref!(ws1, 0, 2),
+        },
+    ))
+}
+
+/*named!(read_pile_size<&[u8], PileSize>,
     do_parse!(
         xz1: le_u8                          >>
         l: le_f32                           >>
@@ -118,8 +153,34 @@ named!(read_pile_size<&[u8], PileSize>,
             ws2: *array_ref!(ws2, 0 ,2)
         })
     )
-);
-named_args!(read_pile_type(type_pile: u8)<&[u8], PileType>,
+);*/
+pub fn read_pile_size(i: &[u8]) -> IResult<&[u8], PileSize> {
+    let (i, xz1) = le_u8(i)?;
+    let (i, l) = le_f32(i)?;
+    let (i, xz2) = le_u8(i)?;
+    let (i, broaden) = le_f32(i)?;
+    let (i, k) = le_f32(i)?;
+    let (i, ws1) = take(9u8)(i)?;
+    let (i, b) = le_f32(i)?;
+    let (i, h) = le_f32(i)?;
+    let (i, ws2) = take(2u8)(i)?;
+    Ok((
+        i,
+        PileSize {
+            xz1,
+            l,
+            xz2,
+            broaden,
+            k,
+            ws1: *array_ref!(ws1, 0, 9),
+            b,
+            h,
+            ws2: *array_ref!(ws2, 0, 2),
+        },
+    ))
+}
+
+/*named_args!(read_pile_type(type_pile: u8)<&[u8], PileType>,
     do_parse!(
         pile_ef: cond!(type_pile   == 1,
             read_pile_ef)                   >>
@@ -135,8 +196,26 @@ named_args!(read_pile_type(type_pile: u8)<&[u8], PileType>,
             }
         )
     )
-);
-named!(pub read_pile<&[u8], Pile>,
+);*/
+pub fn read_pile_type(i: &[u8], type_sec: u8) -> IResult<&[u8], PileType> {
+    match type_sec {
+        1 => {
+            let (i, pile_ef) = read_pile_ef(i)?;
+            Ok((i, PileType::EF(pile_ef)))
+        }
+        2 => {
+            let (i, pile_f_l) = read_pile_f_l(i)?;
+            Ok((i, PileType::FL(pile_f_l)))
+        }
+        3 => {
+            let (i, pile_size) = read_pile_size(i)?;
+            Ok((i, PileType::Size(pile_size)))
+        }
+        _ => panic!("type_pile error"),
+    }
+}
+
+/*named!(pub read_pile<&[u8], Pile>,
     do_parse!(
         ws1: take!(2)                       >>
         p: read_point                       >>
@@ -151,4 +230,21 @@ named!(pub read_pile<&[u8], Pile>,
             base
         })
     )
-);
+);*/
+pub fn read_pile(i: &[u8]) -> IResult<&[u8], Pile> {
+    let (i, ws1) = take(2u8)(i)?;
+    let (i, p) = read_point(i)?;
+    let (i, type_pile) = le_u8(i)?;
+    let (i, ws2) = take(15u8)(i)?;
+    let (i, base) = read_pile_type(i, type_pile)?;
+    Ok((
+        i,
+        Pile {
+            ws1: *array_ref!(ws1, 0, 2),
+            p,
+            type_pile,
+            ws2: *array_ref!(ws2, 0, 15),
+            base,
+        },
+    ))
+}
