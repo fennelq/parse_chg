526fdd3bdb907c7ca6f6773996968bfdb722d99c
Mikhail Boldyshev <fennelq@gmail.com>
2020-02-28 11:30:40 +0700

    Add slabs highlight

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ mod tests;
 use crate::read_write::{read_file, read_file_raw, write_by_file_raw, write_recognize_sig};
 
 fn main() {
-    let input = Path::new("partition_all.chg");
+    let input = Path::new("slab_1_opening.chg");
     let building_s = read_file_raw(input);
     let building = read_file(input);
     write_by_file_raw(&building_s);
